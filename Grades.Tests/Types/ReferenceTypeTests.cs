b55e389e4a0e437f0aeb55986c95f2283665c749
jdickson35 <jdickson353@gmail.com>
2017-10-09 16:49:41 -0500

    W10-9-17

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -11,6 +11,40 @@ namespace Grades.Tests.Types
     public class TypeTests
     {
         [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+            grades[2] = 51.7f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+        [TestMethod]
         public void ValueTypepassByValue()
         {
             int x = 46;
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -12,15 +12,45 @@ namespace Grades
         {
             
             GradeBook book = new GradeBook();
+
+            book.NameChanged += (OnNameChanged);
+            book.NameChanged += (OnNameChanged2);
+            book.NameChanged += (OnNameChanged2);
+            book.NameChanged -= (OnNameChanged2);
+
+            book.Name = "Scott's Grade Book";
+            book.Name = "Justin's Grade Book";
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine("Average grade = " + stats.AverageGrade);
-            Console.WriteLine("Highest grade = " + stats.HighestGrade);
-            Console.WriteLine("Lowest grade = " + stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
             
+        }
+
+        static void OnNameChanged(string existingName, string newName)
+        {
+            Console.WriteLine($"Grade book changing from {existingName} to {newName}");
+        }
+
+        static void OnNameChanged2(string existingName, string newName)
+        {
+            Console.WriteLine("***");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
         }
     }
 }
